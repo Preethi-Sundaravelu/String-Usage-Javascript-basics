fc35617a477635319d59b7350932331de08ed1af
Preethi KCS <99183346+Preethi-Sundaravelu@users.noreply.github.com>
2022-03-17 21:44:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var clg="Sastra Deemed University"
+var len=clg.length
+console.log("Length of string: "+len)
+let cap=clg.toUpperCase()
+console.log("String "+clg+" to uppercase "+cap)
+console.log("Index of letter a: "+clg.indexOf("a"))
+console.log("Famous Conversation")
+console.log("Peter Parker/Spider-Man: That thing does not obey the laws of physics at all")
